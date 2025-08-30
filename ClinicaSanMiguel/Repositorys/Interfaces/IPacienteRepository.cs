47c12059d545267ac03690d63e53041882652bfc
Abel Sulca Espinoza <abelsulcaespinoza@gmail.com>
2025-08-30 18:03:30 -0500

    SelectLoginRegister

--- ClinicaSanMiguel/Repositorys/Interfaces/IPacienteRepository.cs
+++ ClinicaSanMiguel/Repositorys/Interfaces/IPacienteRepository.cs
@@ -9,9 +9,6 @@ namespace ClinicaSanMiguel.Repositorys.Interfaces
         Task<GeneralResponseDto> RegisterAsync(RegisterRequestDto request);
         Task<GeneralResponseDto> UpdateProfileAsync(UpdateProfileRequestDto request);
         Task<GeneralResponseDto> AddFamiliarAsync(AddFamiliarRequestDto request);
-<<<<<<< HEAD
-=======
         Task<ProfileResponseDto> LoadingProfileAsync(int idPaciente);
->>>>>>> ebb87ad9ededa54f61699bfa18c097b9863cf2a9
     }
 }
