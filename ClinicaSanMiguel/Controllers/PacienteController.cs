251ebbd990449953c317b7d9b66cf4cde9f71e17
Abel Sulca Espinoza <abelsulcaespinoza@gmail.com>
2025-08-30 18:53:40 -0500

    profile copntroller paciente

--- ClinicaSanMiguel/Controllers/PacienteController.cs
+++ ClinicaSanMiguel/Controllers/PacienteController.cs
@@ -129,18 +129,5 @@ namespace ClinicaSanMiguel.Controllers
             return View();
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Profile(int idPaciente)
-        {
-            var profile = await _pacienteRepository.LoadingProfileAsync(idPaciente);
-
-            if (profile == null || profile.IdPaciente == 0)
-            {
-                ViewBag.Error = "No se encontró el perfil del paciente.";
-                return RedirectToAction("Login"); // o a donde quieras redirigir
-            }
-
-            return View(profile); // le mandamos el modelo a la vista
-        }
     }
 }
